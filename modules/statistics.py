689568a7c480084734de9d1c46078da19cab1588
Jonathan Poblete OFRHMA <jonathan.e.poblete.c@gmail.com>
2024-07-13 21:29:53 -0400

    merge

---
+++ modules/statistics.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+class Statistics:
+    @staticmethod
+    def calculate(variable_data: pd.Series) -> pd.Series:
+        # Implementation of calculate_statistics
+
+    @staticmethod
+    def combine(stats1: pd.Series, stats2: pd.Series) -> pd.Series:
+        # Implementation of combine_statistics
+
+    @staticmethod
+    def normalize(stat_value: float, stat_name: str, table: pd.DataFrame) -> float:
+        # Implementation of normalize_statistics
