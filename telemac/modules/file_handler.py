21a71a21f76adc55837a4e1c3e797e229b97f2a1
Jonathan <jonny1618@gmail.com>
2024-08-22 00:44:44 -0400

    Cambios en archivos para correr telemac y procesar resultados

--- telemac/modules/file_handler.py
+++ telemac/modules/file_handler.py
@@ -1,5 +1,6 @@
 import os
 from modules.file_utils import move_file
+from loguru import logger
 
 def add_continuation_lines(steering_file, i):
     """
@@ -32,6 +33,7 @@ def add_continuation_lines(steering_file, i):
 
     with open(steering_file, 'w') as f:
         f.writelines(content)
+    logger.info(f"Continuation lines added to file {steering_file}")
 
 def update_duration(steering_file):
     """
@@ -50,7 +52,7 @@ def update_duration(steering_file):
         content = f.readlines()
 
     for idx, line in enumerate(content):
-        if line.strip().startswith("DURATION ="):
+        if line.strip().startswith("DURATION"):
             current_duration = int(line.split("=")[1].strip())
             new_duration = current_duration + 60
             content[idx] = f"DURATION = {new_duration}\n"
@@ -58,6 +60,7 @@ def update_duration(steering_file):
 
     with open(steering_file, 'w') as f:
         f.writelines(content)
+    logger.info(f"Duration updated to {new_duration} s in file {steering_file}")
 
 def prepare_steering_file(src_file, dst_file, result_file, i, continue_simulation):
     """
@@ -80,9 +83,9 @@ def prepare_steering_file(src_file, dst_file, result_file, i, continue_simulatio
     --------
     >>> prepare_steering_file('src.txt', 'dst.txt', 'result.txt', 1, True)
     """
-    move_file(src_file, dst_file)
     if os.path.exists(result_file):
         if continue_simulation:
-            add_continuation_lines(dst_file, i)
+            add_continuation_lines(src_file, i)
         else:
-            update_duration(dst_file)
+            update_duration(src_file)
+    move_file(src_file, dst_file)
